f6462f165f1f8b3154e9926166f7a958e7ab2a81
totoro-fly <liujian180389@gmail.com>
2017-01-23 22:31:37 +0800

    增加网络功能

--- app/src/main/java/com/totoro_fly/earthquake/EarthquakeActivity.java
+++ app/src/main/java/com/totoro_fly/earthquake/EarthquakeActivity.java
@@ -2,12 +2,14 @@ package com.totoro_fly.earthquake;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
+import java.net.URL;
 import java.util.ArrayList;
 
 import butterknife.Bind;
@@ -18,14 +20,40 @@ public class EarthquakeActivity extends AppCompatActivity {
 
     @Bind(R.id.list_view_activity_earthquake)
     ListView listViewActivityEarthquake;
+    private static final String USGS_REQUEST_URL = "http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&orderby=time&minmag=6&limit=10";
+    EarthquakeAsyncTask earthquakeAsyncTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_earthquake);
         ButterKnife.bind(this);
-        final ArrayList<Earthquake> earthquakesArrayList = QueryUtils.extractEarthquakes();
-        final EarthquakeAdapter earthquakeAdapter = new EarthquakeAdapter(this, earthquakesArrayList);
+        earthquakeAsyncTask = new EarthquakeAsyncTask();
+        earthquakeAsyncTask.execute(USGS_REQUEST_URL);
+    }
+
+    private class EarthquakeAsyncTask extends AsyncTask<String, Void, ArrayList<Earthquake>> {
+
+        @Override
+        protected ArrayList doInBackground(String... urls) {
+            if (urls.length < 1 || urls[0] == null)
+                return null;
+            URL url = QueryUtils.createUrl(urls[0]);
+            String jsonResponse = QueryUtils.makeHTTPRequst(url);
+            ArrayList<Earthquake> earthquakesList = QueryUtils.extractEarthquakes(jsonResponse);
+            return earthquakesList;
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<Earthquake> earthquakeList) {
+            if (earthquakeList == null)
+                return;
+            updateUI(earthquakeList);
+        }
+    }
+
+    private void updateUI(ArrayList<Earthquake> earthquakeArrayList) {
+        final EarthquakeAdapter earthquakeAdapter = new EarthquakeAdapter(this, earthquakeArrayList);
         listViewActivityEarthquake.setAdapter(earthquakeAdapter);
         listViewActivityEarthquake.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
